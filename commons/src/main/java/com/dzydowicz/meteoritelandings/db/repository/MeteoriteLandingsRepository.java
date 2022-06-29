34dd193ed841c2ed5e6242fbfcd0cdbf5876277c
damianzydowicz <damian.zydowicz@gmail.com>
2022-06-29 18:50:12 +0200

    Database interface

---
+++ commons/src/main/java/com/dzydowicz/meteoritelandings/db/repository/MeteoriteLandingsRepository.java
@@ -0,0 +1,17 @@
+package com.dzydowicz.meteoritelandings.db.repository;
+
+import com.dzydowicz.meteoritelandings.models.MeteoriteLandingTO;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface MeteoriteLandingsRepository extends MongoRepository<MeteoriteLandingTO, String> {
+
+    Optional<MeteoriteLandingTO> findById(int id);
+
+    boolean existsById(int id);
+
+    void deleteById(int id);
+
+    int findWithHighestId();
+}

d78c389fcabc14a6b1dbffcd45b3c74732472fe6
damianzydowicz <damian.zydowicz@gmail.com>
2022-09-20 19:24:37 +0200

    Provided main for SOAP API application

---
+++ soap-api/src/main/java/com/dzydowicz/meteoritelandings/SoapApiApplication.java
@@ -0,0 +1,14 @@
+package com.dzydowicz.meteoritelandings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@EnableMongoRepositories
+public class SoapApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapApiApplication.class, args);
+    }
+}
